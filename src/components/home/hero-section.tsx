b447184f679aca3472501a804bb4b82b49e49dd4
Subham1901 <subhamprasad1901@gmail.com>
2025-11-17 22:52:53 +0530

    style: hero section mobile view

--- src/components/home/hero-section.tsx
+++ src/components/home/hero-section.tsx
@@ -5,19 +5,20 @@ import Link from "next/link";
 
 import SlideUp from "../animations/slide-up";
 import PhoneIconOutlined from "../common/icons/phone";
+import { RotatingText } from "../ui/rotating-text";
 
 export default function Hero() {
   return (
-    <section className="relative w-full overflow-hidden bg-bg-light py-2 md:py-28">
+    <section className="relative w-full overflow-hidden bg-bg-light md:py-28">
       {/* Background Soft Glows */}
 
-      <div className="relative mx-auto max-w-7xl px-3 grid grid-cols-1 bg-accent/30 rounded-3xl md:grid-cols-2 gap-12 items-center">
+      <div className="relative mx-auto max-w-7xl px-3 grid grid-cols-1 md:bg-accent/30 rounded-3xl md:grid-cols-2 gap-12 items-center">
         {/* Text + CTA */}
         <div className="relative">
           <div className="relative flex flex-col items-center md:items-start space-y-6  md:p-10">
             {/* Heading */}
             <SlideUp>
-              <h1 className="font-playfair text-center sm:text-left font-heading text-3xl md:text-5xl lg:text-6xl text-primary leading-tight">
+              <h1 className="font-playfair mt-12 md:mt-0 text-center sm:text-left font-heading text-5xl md:text-5xl lg:text-6xl text-primary leading-tight">
                 Contour Cosmetic Clinic
               </h1>
             </SlideUp>
@@ -33,10 +34,19 @@ export default function Hero() {
 
             {/* Accent Line */}
             <SlideUp>
-              <p className="text-forest-light text-center sm:text-left text-sm font-body font-semibold tracking-wide">
+              <p className="text-forest-light hidden md:block text-center sm:text-left text-sm font-body font-semibold tracking-wide">
                 Hair Treatment • Skin Rejuvenation • Facial Plastics • Laser
                 Resurfacing
               </p>
+              <RotatingText
+                className="text-xl font-heading md:hidden block"
+                text={[
+                  "• Hair Treatment •",
+                  "• Skin Rejuvenation •",
+                  "• Facial Plastics •",
+                  "• Laser Resurfacing •",
+                ]}
+              />
             </SlideUp>
 
             <SlideUp>
@@ -63,19 +73,21 @@ export default function Hero() {
 
         {/* Hero Image */}
         <SlideUp>
-          <div className="relative flex justify-center md:justify-end">
-            {/* Glow */}
-            <div className="absolute inset-0 bg-[--color-beige-light]/30 blur-3xl rounded-full scale-150"></div>
+          <div className="hidden md:block">
+            <div className="relative flex justify-center md:justify-end">
+              {/* Glow */}
+              <div className="absolute inset-0 bg-[--color-beige-light]/30 blur-3xl rounded-full scale-150"></div>
 
-            <div className="relative w-fit -mt-12">
-              <Image
-                loading="lazy"
-                src="/images/hero-colseup.png"
-                height={100} // increase from 1020 → 1200 or even 1400
-                width={500}
-                alt="Cosmetic Model"
-                className="rounded-3xl object-contain transform transition-all duration-500 filter drop-shadow-[6px_6px_10px_rgba(0,0,0,0.2)]"
-              />
+              <div className="relative w-fit -mt-12">
+                <Image
+                  loading="lazy"
+                  src="/images/hero-colseup.png"
+                  height={100} // increase from 1020 → 1200 or even 1400
+                  width={500}
+                  alt="Cosmetic Model"
+                  className="rounded-3xl object-contain transform transition-all duration-500 filter drop-shadow-[6px_6px_10px_rgba(0,0,0,0.2)]"
+                />
+              </div>
             </div>
           </div>
         </SlideUp>
