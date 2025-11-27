c56ecf77a964c0a3f4d92fc4902464dd24d9652a
Subham1901 <subhamprasad1901@gmail.com>
2025-11-27 17:14:55 +0530

    remove doct

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -183,15 +183,15 @@ export const doctors: DoctorType[] = [
       "Dr. Saket is a renowned facial cosmetic surgeon with specialized expertise in FUE hair transplantation. With a pan-India presence, he performs advanced hair restoration procedures from North to South India and serves as the leading surgeon for several reputed cosmetic chains in Bangalore. A graduate of Rajiv Gandhi University of Health Sciences, Dr. Saket has undergone intensive training and has worked alongside some of the most esteemed doctors in India. His clinical journey spans premier institutions such as Tata Medical Center (Kolkata), RCC Trivandrum, Jubilee Mission Medical College (Thrissur), Karnataka Institute of Medical Sciences (KIMS), and NIMHANS (Bangalore), among others. Known for blending surgical precision with artistic vision, Dr. Saket brings a decade of experience in facial aesthetics, reconstructive surgery, and advanced hair restoration, making him a trusted name in the field of facial transformation and cosmetic excellence.",
     image: "/images/doctors/saket-jha.JPG",
   },
-  {
-    id: 2,
-    name: "Dr. Shambhavi Shukla",
-    title: "MS(DNB) ENT Head & Neck oncosurgery (FELLOW)",
-    //   experience: "10+ Years of Experience",
-    description:
-      "Dr. Shambhavi Shukla is a renowned ENT surgeon with specialized fellowship training in Head and Neck Oncology from Tata Medical Center, combining expertise in oncologic surgery with a strong focus on facial reconstruction. Recognized for her ability to provide comprehensive care in complex head and neck cancer cases, she excels in both the surgical management of tumors and the restoration of facial features, enabling patients to regain not only their health but also their appearance and confidence. A graduate of Command Hospital, Kolkata, Dr. Shambhavi completed her postgraduate studies (MS) in ENT, honing her skills in one of India’s most prestigious military healthcare institutions. At Tata Medical Center, she further specialized in head and neck oncology, refining her proficiency in state-of-the-art surgical and reconstructive techniques for facial restoration. Her compassionate approach and dedication to advanced reconstructive methods, including the integration of aesthetic principles, make Dr. Shambhavi a leader in the field of head and neck onco. She combines cutting-edge technologies with a deep understanding of facial anatomy to provide life-changing results for patients undergoing head and neck surgeries.",
-    image: "/images/doctors/shambhavi-shukla.PNG",
-  },
+  // {
+  //   id: 2,
+  //   name: "Dr. Shambhavi Shukla",
+  //   title: "MS(DNB) ENT Head & Neck oncosurgery (FELLOW)",
+  //   //   experience: "10+ Years of Experience",
+  //   description:
+  //     "Dr. Shambhavi Shukla is a renowned ENT surgeon with specialized fellowship training in Head and Neck Oncology from Tata Medical Center, combining expertise in oncologic surgery with a strong focus on facial reconstruction. Recognized for her ability to provide comprehensive care in complex head and neck cancer cases, she excels in both the surgical management of tumors and the restoration of facial features, enabling patients to regain not only their health but also their appearance and confidence. A graduate of Command Hospital, Kolkata, Dr. Shambhavi completed her postgraduate studies (MS) in ENT, honing her skills in one of India’s most prestigious military healthcare institutions. At Tata Medical Center, she further specialized in head and neck oncology, refining her proficiency in state-of-the-art surgical and reconstructive techniques for facial restoration. Her compassionate approach and dedication to advanced reconstructive methods, including the integration of aesthetic principles, make Dr. Shambhavi a leader in the field of head and neck onco. She combines cutting-edge technologies with a deep understanding of facial anatomy to provide life-changing results for patients undergoing head and neck surgeries.",
+  //   image: "/images/doctors/shambhavi-shukla.PNG",
+  // },
   {
     id: 3,
     name: "Dr. Akanksha Thakur",
