05a86191858bd19b8747f15edffc222564544ecd
Subham1901 <subhamprasad1901@gmail.com>
2025-11-30 08:33:56 +0530

    remove use client from service page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Metadata } from "next";
 import ServicesWithFilter from "../../components/servicesWithFilter";
 import { serviceMetadata } from "../../lib/metadata/serviceMetaData";
