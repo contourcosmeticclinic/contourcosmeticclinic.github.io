d91355d58f781b9e446a1e815408894e54639914
Subham1901 <subhamprasad1901@gmail.com>
2025-12-19 11:01:52 +0530

    [GTM_EVENT] add GTM event

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -79,7 +79,47 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NDC36237');
+            `,
+          }}
+        />
+        {/* Google Ads / gtag */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17041035401"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="gtag-config"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17041035401');
+            `,
+          }}
+        />
+      </head>
       <body className={`${inter.variable} ${playfair.variable} antialiased bg-bg-light`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NDC36237"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Script
           id="wf_anal"
           src="https://crm.zohopublic.in/crm/WebFormAnalyticsServeServlet?rid=9ee5d4a7dc28eb312eac81842770150719df078bd77229ff1a59c151e24ea6dc956e87710770e84df354d80f12fa82d7gid5d202dff8d82b9f3db58189ead9c6f79f822dc7371c2dab06fdc6ddc6e094c7egid046cb25b2d23f3f4a2d727931ffcd565693a5363c651d71e88ac16d423efe708gid5b87600ccace1db5fe88558e8b280b85b235e8ddc199c416468223c62977fb11&tw=7bea06a95a55c235be4586f2fd5640cdf4302b907beaa534d434f655509e06e2"
