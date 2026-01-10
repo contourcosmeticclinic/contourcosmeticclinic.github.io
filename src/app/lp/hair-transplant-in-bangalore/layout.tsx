26580477e04915fd90918e81c18c644a35dde451
Subham1901 <subhamprasad1901@gmail.com>
2026-01-10 22:21:23 +0530

    [vefified]

--- src/app/lp/hair-transplant-in-bangalore/layout.tsx
+++ src/app/lp/hair-transplant-in-bangalore/layout.tsx
@@ -2,12 +2,10 @@ import { GoogleTagManager } from "@next/third-parties/google";
 
 export default function HairTransplantLayout({ children }: { children: React.ReactNode }) {
   const GTM_ID = process.env.NEXT_PUBLIC_LP_GTM_ID;
-  console.warn("GTM ID verified", GTM_ID);
 
   return (
     <>
       {GTM_ID && <GoogleTagManager gtmId={GTM_ID} />}
-      <div>{GTM_ID}</div>
       {children}
     </>
   );
